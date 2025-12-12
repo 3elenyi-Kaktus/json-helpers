1cf4072323c08523221765217418de4d48b0f51c
Fedor Fatkullin <myhare.ff@gmail.com>
2025-12-12 17:04:31 +0300

    Clean

---
+++ lib/json/helpers.py
@@ -0,0 +1,29 @@
+import json
+from pathlib import Path
+from typing import Any, TypeAlias
+
+
+__version__ = "0.1.1"
+
+JSON: TypeAlias = dict[str, Any] | list
+
+
+def readJSON(filepath: Path) -> JSON:
+    if not filepath.suffix == ".json":
+        raise RuntimeError(f"JSON Helper: File '{filepath}' is not a JSON file")
+    if not filepath.is_file():
+        raise RuntimeError(f"JSON Helper: File '{filepath}' does not exist")
+    with open(filepath, mode="rt", newline="\n") as jsonfile:
+        contents: JSON = json.load(jsonfile)
+    return contents
+
+
+def writeJSON(filepath: Path, contents: JSON, sort: bool = False) -> None:
+    if not isinstance(contents, JSON):
+        raise RuntimeError(f"JSON Helper: Expected: '{JSON}' to write, got: {type(contents)}")
+    with open(filepath, mode="wt", newline="\n") as jsonfile:
+        jsonfile.write(json.dumps(contents, ensure_ascii=False, indent=4, sort_keys=sort))
+
+
+def toReadableJSON(contents: dict | list, sort: bool = False) -> str:
+    return json.dumps(contents, ensure_ascii=False, indent=4, sort_keys=sort)
