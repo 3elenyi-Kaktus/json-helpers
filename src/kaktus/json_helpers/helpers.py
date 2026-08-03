bcbb56cb5a51639abc4f9e88d609a6eb938d4ec8
Fedor Fatkullin <myhare.ff@gmail.com>
2026-08-03 22:07:52 +0300

    Minor cleanups

--- src/kaktus/json_helpers/helpers.py
+++ src/kaktus/json_helpers/helpers.py
@@ -11,7 +11,7 @@ def readJSON(filepath: Path) -> JSON:
         raise RuntimeError(f"JSON Helper: File '{filepath}' is not a JSON file")
     if not filepath.is_file():
         raise RuntimeError(f"JSON Helper: File '{filepath}' does not exist")
-    with open(filepath, mode="rt", newline="\n") as jsonfile:
+    with open(filepath, newline="\n") as jsonfile:
         contents: JSON = json.load(jsonfile)
     return contents
 
@@ -19,7 +19,7 @@ def readJSON(filepath: Path) -> JSON:
 def writeJSON(filepath: Path, contents: JSON, *, sort: bool = False) -> None:
     if not isinstance(contents, dict | list):
         raise RuntimeError(f"JSON Helper: Expected: '{JSON}' to write, got: {type(contents)}")
-    with open(filepath, mode="wt", newline="\n") as jsonfile:
+    with open(filepath, mode="w", newline="\n") as jsonfile:
         jsonfile.write(json.dumps(contents, ensure_ascii=False, indent=2, sort_keys=sort))
 
 
